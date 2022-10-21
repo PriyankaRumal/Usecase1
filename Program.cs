9253edbb8cf71f3178f2c4beff04be82db2fa63d
PriyankaRumal <priyankarumal182000@gmail.com>
2022-10-21 11:20:07 +0530

    file added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int value=random.Next(0,2);
+            if(value==0)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+        }
+    }
+}
